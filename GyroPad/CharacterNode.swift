afb64c1af11806e855d79bc1071ec9c2d7e44473
Jake <jakef@fastmail.com>
2019-06-25 07:28:00 -0500

    first version with walls

---
+++ GyroPad/CharacterNode.swift
@@ -0,0 +1,15 @@
+//
+//  CharacterNode.swift
+//  GyroPad
+//
+//  Created by Jake Foster on 6/24/19.
+//  Copyright © 2019 Jake Foster. All rights reserved.
+//
+
+import SpriteKit
+
+class CharacterNode: SKSpriteNode {
+    convenience init(size: CGSize) {
+        self.init(color: .blue, size: size)
+    }
+}
